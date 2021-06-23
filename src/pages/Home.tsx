f2008bb33f6af0cb017c682f08254aa4030a35c7
Guilherme Athayde <guiathayde303@gmail.com>
2021-06-23 12:15:20 -0300

    Class 03 completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,21 @@
+import { useState, FormEvent } from 'react';
 import { useHistory } from 'react-router-dom';
+
+import { database } from '../services/firebase';
+
+import { useAuth } from '../hooks/useAuth';
 
 import { Button } from '../components/Button';
 
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
-
 import '../styles/auth.scss';
-import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -21,33 +25,53 @@ export function Home() {
     history.push('/rooms/new');
   }
 
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
   return (
-    <div id="page-auth">
+    <div id='page-auth'>
       <aside>
-        <img src={ilustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={ilustrationImg}
+          alt='Ilustração simbolizando perguntas e respostas'
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
 
       <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Letmeask" />
-          <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
+        <div className='main-content'>
+          <img src={logoImg} alt='Letmeask' />
+          <button onClick={handleCreateRoom} className='create-room'>
+            <img src={googleIconImg} alt='Logo do Google' />
             Crie sua sala com o Google
           </button>
-          <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input 
-              type="text" 
-              placeholder="Digite o código da sala"
+          <div className='separator'>ou entre em uma sala</div>
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type='text'
+              placeholder='Digite o código da sala'
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
             />
-            <Button type="submit">
-              Entrar na sala
-            </Button>
+            <Button type='submit'>Entrar na sala</Button>
           </form>
         </div>
       </main>
     </div>
-  )
+  );
 }
